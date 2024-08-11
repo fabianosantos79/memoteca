b50a624cfdd3635089115a302be452c00a650501
Fabiano <fabiano.ssantos79@gmail.com>
2024-08-11 12:46:25 -0300

    feat: finish search module

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -14,6 +14,7 @@ export class PensamentoService {
    }
 
    listar(pagina: number, filtro: string): Observable<IPensamento[]>{
+
     const itensPorPagina: number = 6;
 
     let params = new HttpParams()
@@ -21,7 +22,7 @@ export class PensamentoService {
     .set("_limit", itensPorPagina);
 
     if(filtro.trim().length > 2){
-      params.set("q", filtro);
+      params = params.set("q", filtro);
     }
 
     return this.http.get<IPensamento[]>(this.API, {params});
